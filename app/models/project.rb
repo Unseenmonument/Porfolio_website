5f15b4a5eac85e97817de82924ab521d088ecf28
Kerron S. Streater <kerronstreater@gmail.com>
2019-10-31 13:31:48 -0400

    unsecure items on page, hoping to fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,15 @@
 class Project < ApplicationRecord
 
-	has_attached_file :main_pic, default_url: "/images/:style/missing.png"
+	has_attached_file :main_pic, default_url: "/images/:style/missing.png", s3_permissions: :private, s3_server_side_encryption: "AES256"
 	validates_attachment_content_type :main_pic, content_type: /\Aimage\/.*\z/
 
-	has_attached_file :pic1, default_url: "/images/:style/missing.png"
+	has_attached_file :pic1, default_url: "/images/:style/missing.png", s3_permissions: :private, s3_server_side_encryption: "AES256"
 	validates_attachment_content_type :pic1, content_type: /\Aimage\/.*\z/
 
-	has_attached_file :pic2, default_url: "/images/:style/missing.png"
+	has_attached_file :pic2, default_url: "/images/:style/missing.png", s3_permissions: :private, s3_server_side_encryption: "AES256"
 	validates_attachment_content_type :pic2, content_type: /\Aimage\/.*\z/
 
-	has_attached_file :pic3, default_url: "/images/:style/missing.png"
+	has_attached_file :pic3, default_url: "/images/:style/missing.png", s3_permissions: :private, s3_server_side_encryption: "AES256"
 	validates_attachment_content_type :pic3, content_type: /\Aimage\/.*\z/
 
 
